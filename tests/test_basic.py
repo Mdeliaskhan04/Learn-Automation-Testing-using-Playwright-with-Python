3c17275f1b9a94fd8a82f61cc80e52af935cb44e
Mdeliaskhan04 <Mdeliaskhan1918004@gmail.com>
2025-06-22 01:18:32 +0600

    Fixed the error

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -2,7 +2,7 @@ from playwright.sync_api import sync_playwright
 
 def test_basic():
     with sync_playwright() as p:
-        browser = p.firefox.launch(headless=False)
+        browser = p.firefox.launch(headless=True)
         page = browser.new_page()
         page.goto("https://www.google.com")
         print(page.title())
