44051f57a12e1bbbfcdd1422066d85c15124c45c
Mdeliaskhan04 <Mdeliaskhan1918004@gmail.com>
2025-06-22 00:30:37 +0600

    login test cases added for a website

---
+++ tests/test_basic.py
@@ -0,0 +1,10 @@
+from playwright.sync_api import sync_playwright
+
+def test_basic():
+    with sync_playwright() as p:
+        browser = p.firefox.launch(headless=False)
+        page = browser.new_page()
+        page.goto("https://www.google.com")
+        print(page.title())
+        assert "Google" in page.title()
+        browser.close()
---
+++ tests/test_login.py
@@ -0,0 +1,47 @@
+import pytest
+from playwright.sync_api import Page
+
+def test_valid_login(page: Page):
+    page.goto("https://practicetestautomation.com/practice-test-login/")
+    page.fill("input[name='username']","student")
+    page.fill("input[name='password']","Password123")
+    page.screenshot(path="screenshot1.png")
+    page.click("#submit")
+    assert "Logged In Successfully" in page.text_content("h1")
+    page.screenshot(path="screenshot2.png")
+
+def test_invalid_username(page: Page):
+    page.goto("https://practicetestautomation.com/practice-test-login/")
+    page.fill("input[name='username']","invalid_username")
+    page.fill("input[name='password']","Password123")
+    page.screenshot(path="screenshot3.png")
+    page.click("#submit")
+    assert "invalid" in page.text_content("#error").lower()
+    page.screenshot(path="screenshot4.png")
+
+def test_invalid_password(page: Page):
+    page.goto("https://practicetestautomation.com/practice-test-login/")
+    page.fill("input[name='username']","student")
+    page.fill("input[name='password']","invalid_password")
+    page.screenshot(path="screenshot5.png")
+    page.click("#submit")
+    assert "invalid" in page.text_content("#error").lower()
+    page.screenshot(path="screenshot6.png")
+
+def test_empty_username(page: Page):
+    page.goto("https://practicetestautomation.com/practice-test-login/")
+    page.fill("input[name='username']","")
+    page.fill("input[name='password']","Password123")
+    page.screenshot(path="screenshot7.png")
+    page.click("#submit")
+    assert "invalid" in page.text_content("#error").lower()
+    page.screenshot(path="screenshot8.png")
+
+def test_empty_password(page: Page):
+    page.goto("https://practicetestautomation.com/practice-test-login/")
+    page.fill("input[name='username']","student")
+    page.fill("input[name='password']","")
+    page.screenshot(path="screenshot9.png")
+    page.click("#submit")
+    assert "invalid" in page.text_content("#error").lower()
+    page.screenshot(path="screenshot10.png")
