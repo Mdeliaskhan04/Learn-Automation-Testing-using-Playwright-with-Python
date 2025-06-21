95db66b1b28a5162a6fcf08594bd242668b3462b
Mdeliaskhan04 <Mdeliaskhan1918004@gmail.com>
2025-06-21 21:58:45 +0600

    First Commit

---
+++ tests/test_example.py
@@ -0,0 +1,11 @@
+# tests/test_example.py
+
+from playwright.sync_api import sync_playwright
+
+def test_example():
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=False)
+        page = browser.new_page()
+        page.goto("https://www.google.com")
+        assert "Google" in page.title()
+        browser.close()
